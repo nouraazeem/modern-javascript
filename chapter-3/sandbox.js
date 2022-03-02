b23bdb9ac3730aa4e65c7c597a71ff091aeaaaef
Noura <noura.azeem@gmail.com>
2022-03-01 22:33:41 -0500

    commit switch statements from net ninja tutorial

--- chapter-3/sandbox.js
+++ chapter-3/sandbox.js
@@ -96,3 +96,26 @@ for(let i = 0; i < scores.length; i++){
         break;
     }
 } */
+
+// switch statements
+/* const grade = 'D';
+
+switch (grade) {
+    case 'A':
+        console.log('you got an A!');
+        break
+    case 'B':
+        console.log('you got a B!');
+        break
+    case 'C':
+        console.log('you got a C!');
+        break
+    case 'D':
+        console.log('you got a D!');
+        break
+    case 'F':
+        console.log('you got an F!');
+        break
+    default:
+        console.log('not a valid grade');
+} */
