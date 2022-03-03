1a38eb8f4955aaaa06e1fe619db0b06aa2c65374
Noura <noura.azeem@gmail.com>
2022-03-02 20:54:43 -0500

    using foreach and callback function

--- chapter-4/sandbox.js
+++ chapter-4/sandbox.js
@@ -83,3 +83,55 @@ console.log(bill([10,15,30], 0.2));
 console.log(bill([10,15,30], 0.2));
  */
 
+/* const name = 'shaun';
+
+// functions
+
+const greet = () => 'hello';
+
+let resultOne = greet();
+console.log(resultOne);
+
+// methods
+let resultTwo = name.toUpperCase();
+console.log(resultTwo); */
+
+// callbacks & foreach
+
+/* const myFunc = (callbackFunc) => {
+    // do something
+    let value = 50;
+    callbackFunc(value);
+};
+
+myFunc(value => {
+    // do something
+    console.log(value);
+})
+ */
+/* let people = ['mario', 'luigi', 'ryu', 'shaun', 'noura']
+
+const logPerson = (person, index) => {
+    console.log(`${index} - hello ${person}`)
+}
+
+people.forEach(logPerson);
+ */
+/* people.forEach((person, index) => {
+    console.log('something', person, index);
+}); */
+
+// get a reference to the 'ul'
+const ul = document.querySelector('.people');
+
+const people = ['mario', 'luigi', 'ryu', 'shaun', 'noura'];
+
+let html = ``;
+
+people.forEach(function(person){
+    // create html template
+    html += `<li style="color: purple"> ${person}</li>`;
+});
+
+console.log(html);
+ul.innerHTML = html
