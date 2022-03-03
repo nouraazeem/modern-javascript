818b3b57e11bb02de0a8446739fbc27acc5d9450
Noura <noura.azeem@gmail.com>
2022-03-02 20:35:39 -0500

    arrow functions complete

--- chapter-4/sandbox.js
+++ chapter-4/sandbox.js
@@ -44,7 +44,42 @@ calcVol(area) */
     return 3.14 * radius**2;
 }; */
 
-const calcArea = radius => 3.14 * radius**2;
+/* const calcArea = radius => 3.14 * radius**2;
 
 const area = calcArea(5);
-console.log('area is:', area);
+console.log('area is:', area); */
+
+// practice arrow functions
+
+// before
+/* const greet = function(){
+    return 'hello, world';
+} */
+
+// new
+/* const greet = () => 'hello, world';
+
+const result = greet();
+console.log(result); */
+
+// another example
+/* const bill = function(products, tax){
+    let total = 0;
+    for(let i = 0; i < products.length; i++){
+        total += products[i] + products[i] * tax;
+    } 
+    return total;
+}
+console.log(bill([10,15,30], 0.2));
+ */
+
+/* const bill = (products, tax) => {
+    let total = 0;
+    for(let i = 0; i < products.length; i++){
+        total += products[i] + products[i] * tax;
+    } 
+    return total;
+};
+console.log(bill([10,15,30], 0.2));
+ */
+
