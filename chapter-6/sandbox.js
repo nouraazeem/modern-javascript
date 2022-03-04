221c2bc7581c7c0a17d3e3655348c9d5c1f0a8db
Noura <noura.azeem@gmail.com>
2022-03-03 20:55:18 -0500

    ch 6 learning about getting elements

--- chapter-6/sandbox.js
+++ chapter-6/sandbox.js
@@ -11,7 +11,15 @@ paras.forEach(para => {
 console.log(paras[2]); */
 
 // get an element by ID
-
+/* const title = document.getElementById('page-title');
+console.log(title);
+ */
 //get elements by their class name
+/* const errors = document.getElementsByClassName('error')
+console.log(errors);
+console.log(errors[0]);
+ */
 
 // get elements by their tag name
+const paras = document.getElementsByTagName('p');
+console.log(paras[1]);
