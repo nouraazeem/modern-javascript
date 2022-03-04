0eb16edbfc594f2196daa15f027e6bc91109e6b9
Noura <noura.azeem@gmail.com>
2022-03-03 20:50:16 -0500

    ch 6

---
+++ chapter-6/sandbox.js
@@ -0,0 +1,17 @@
+/* const para = document.querySelector('body > h1');
+
+console.log(para); */
+/* 
+const paras = document.querySelectorAll('p');
+
+paras.forEach(para => {
+    console.log(para);
+});
+
+console.log(paras[2]); */
+
+// get an element by ID
+
+//get elements by their class name
+
+// get elements by their tag name
