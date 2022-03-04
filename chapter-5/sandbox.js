2ffca717be0379e672ad7e0760dc2f7b224e8c15
Noura <noura.azeem@gmail.com>
2022-03-03 20:25:27 -0500

    math objects + random numbers

--- chapter-5/sandbox.js
+++ chapter-5/sandbox.js
@@ -15,4 +15,68 @@ console.log(user.age);
 console.log(user['name'])
 
 user['name'] = 'noura';
-console.log(user['name']); */
+console.log(user['name']);
+
+console.log(typeof user); */
+
+/* const blogs = [
+    { title: 'why mac & cheese rules', likes: 30 },
+    { title: '10 things to make with tahina', likes: 50 }
+];
+
+console.log(blogs); */
+/* 
+
+let user = {
+    name: 'crystal',
+    age: 30,
+    email: 'noura@gmail.com',
+    location: 'berlin',
+    blogs: [
+        { title: 'why mac & cheese rules', likes: 30 },
+        { title: '10 things to make with tahina', likes: 50 }
+    ],
+    login: function(){
+        console.log('the user logged in');
+    },
+    logout: function(){
+        console.log('the user logged out')
+    },
+    logBlogs: function(){
+        //console.log(this.blogs);
+        console.log('this user has written the following blogs:');
+        this.blogs.forEach(blog => {
+            console.log(blog.title, blog.likes);
+        })
+    }
+};
+
+user.logBlogs();
+console.log(this);
+
+/* user.login();
+user.logout();
+
+const name = 'mario';
+name.toUpperCase(); */ 
+
+// math object 
+
+/* console.log(Math);
+console.log(Math.PI);
+console.log(Math.E);
+
+const area = 7.7;
+
+console.log(Math.round(area));
+console.log(Math.floor(area));
+console.log(Math.ceil(area));
+console.log(Math.trunc(area));
+
+// random numbers
+
+const random = Math.random();
+
+console.log(random);
+console.log(Math.round(random * 100)); */
+
