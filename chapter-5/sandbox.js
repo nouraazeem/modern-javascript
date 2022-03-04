82fca3b453f36efc6aa28b34577b1d6d22c859b5
Noura <noura.azeem@gmail.com>
2022-03-03 19:52:06 -0500

    ch 5 - objects in js

---
+++ chapter-5/sandbox.js
@@ -0,0 +1,18 @@
+// object literals
+
+/* let user = {
+    name: 'crystal',
+    age: 30,
+    email: 'noura@gmail.com',
+    location: 'berlin',
+    blogs: ['why mac & cheese rules', '10 things to make w tahina']
+};
+
+console.log(user.blogs[1]);
+
+user.age = 35;
+console.log(user.age);
+console.log(user['name'])
+
+user['name'] = 'noura';
+console.log(user['name']); */
